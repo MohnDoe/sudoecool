a4f94d3f7c2be717d1dd1826a1852ea4679329d1
mohndoe <bonjour@doe.cool>
2026-02-12 09:48:54 +0100

    fix: use correct $fetch for token auth

--- app/composables/useDiscordSdk.ts
+++ app/composables/useDiscordSdk.ts
@@ -1,5 +1,6 @@
 
 import { DiscordSDKMock, type Types } from "@discord/embedded-app-sdk";
+import type { FetchError } from 'ofetch';
 
 export const useDiscordSDK = () => {
   useNuxtApp();
@@ -20,32 +21,12 @@ export const useDiscordSDK = () => {
         response_type: 'code',
         state: '',
         prompt: 'none',
-        // More info on scopes here: https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
         scope: [
-          // Activities will launch through app commands and interactions of user-installable apps.
-          // https://discord.com/developers/docs/tutorials/developing-a-user-installable-app#configuring-default-install-settings-adding-default-install-settings
           'applications.commands',
-
-          // "applications.builds.upload",
-          // "applications.builds.read",
-          // "applications.store.update",
-          // "applications.entitlements",
-          // "bot",
           'identify',
-          // "connections",
-          // "email",
-          // "gdm.join",
           'guilds',
-          // "guilds.join",
           'guilds.members.read',
-          // "messages.read",
-          // "relationships.read",
           'rpc.activities.write',
-          // "rpc.notifications.read",
-          // 'rpc.voice.write',
-          // 'rpc.voice.read',
-          // "webhook.incoming",
-          // discordSdk.guildId == null ? 'dm_channels.read' : null, // This scope requires approval from Discord.
         ].filter((scope) => scope != null) as Types.OAuthScopes[],
       });
 
@@ -64,32 +45,31 @@ export const useDiscordSDK = () => {
         }
 
         discordStore.setAuth(mockData);
-
-        // await discordSdk.commands.authenticate({
-        //   access_token: 'mock_access_token'
-        // })
       } else {
-        const { data: tokenData, error } = await useFetch<DiscordTokenResponse>('/api/auth/discord/token', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            code,
-          }),
-        });
-        if (!error) {
-          console.error(`Error fetching token`, error)
-          throw new Error(error || 'Failed to exchange code for token');
+        try {
+          const response = await $fetch<DiscordTokenResponse>('/api/auth/discord/token', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              code,
+            }),
+          });
+
+          discordStore.setAuth(response);
+
+          await discordStore.sdk.commands.authenticate({
+            access_token: response.access_token,
+          });
         }
-        discordStore.setAuth(tokenData.value as DiscordAuth);
+        catch (e) {
+          const error = e as FetchError;
+          console.error(`Error fetching token`, error)
+          throw new Error(error.data || 'Failed to exchange code for token');
+        }
 
-        // Authenticate with Discord SDK
-        await discordStore.sdk.commands.authenticate({
-          access_token: tokenData.value!.access_token,
-        });
       }
-
 
     } catch (err) {
       const message = err instanceof Error ? err.message : 'Unknown error';
