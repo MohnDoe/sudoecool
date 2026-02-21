f0cf70575865ad32a3d13890c21fc93cb1a94dea
mohndoe <bonjour@doe.cool>
2026-02-21 14:36:32 +0100

    error cell design change

--- app/components/sudoku/Cell.vue
+++ app/components/sudoku/Cell.vue
@@ -101,12 +101,12 @@ const cellClasses = computed(() => ({
 .sudoku-cell--error::after {
   position: absolute;
   content: ' ';
-  height: 8px;
-  width: 8px;
-  border-radius: 4px;
+  border: 4px solid var(--ui-error);
+  border-radius: 8px;
   bottom: 4px;
   right: 4px;
-  background-color: var(--ui-error);
+  left: 4px;
+  top: 4px;
 }
 
 .sudoku-cell--selected {
@@ -119,7 +119,12 @@ const cellClasses = computed(() => ({
 }
 
 .sudoku-cell--error.sudoku-cell--selected::after {
-  background-color: white;
+  border-color: white;
+  left: unset;
+  top: unset;
+  right: 4px;
+  bottom: 4px;
+  border-width: 4px;
 }
 
 .sudoku-cell--filled:not(.sudoku-cell--given) {
